8b931fb04a6510fe065643637366440e0a488315
Joe45098 <143167545+Joe45098@users.noreply.github.com>
2023-08-25 02:29:51 -0500

    Update app.js

--- app.js
+++ app.js
@@ -67,7 +67,7 @@ document.getElementById('form').addEventListener('submit', function (event) {
     };
 
     // Send the form data via Email.js
-    emailjs.send("service_gq7an17", "template_xe7frwd", formData)
+    emailjs.send("service_e4sgn2w", "template_iy0uhkt", formData)
         .then(function (response) {
             console.log('SUCCESS!', response.status, response.text);
             redirectToURL();
